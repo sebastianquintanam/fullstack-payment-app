1de8bed926c37253a16295c574d0b2b2aa9049a3
Sebastian Quintana Morales <104689303+sebastianquintanam@users.noreply.github.com>
2024-12-29 16:59:35 -0500

    Added validations, input mask, and Wompi integration simulation in PaymentPage
    - Added validation for card number, CVV, and expiry date.
    
    - Implemented input mask for expiry date field.
    
    - Simulated payment process with Wompi.
    
    - Added a Cancel button for better UX.

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+// Endpoint para "/products"
+app.get("/", (req, res) => {
+    res.send("Bienvenido al backend. Usa /products para obtener los datos.");
+  });
+  
+app.get("/products", (req, res) => {
+    console.log("Request received at /products");
+    res.json([
+      { id: 1, name: "Product 1", description: "Description for product 1", price: 100 },
+      { id: 2, name: "Product 2", description: "Description for product 2", price: 200 },
+    ]);
+  });  
+
+// Iniciar el servidor
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Backend running on http://localhost:${PORT}`);
+});
